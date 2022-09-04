tequilaOS/platform_vendor_tequila

vendor: Import Pixel bootanimation
Change-Id: Id32cc5e859d5930e5cd81f9d8ab375cbec947311

config/common.mk
@@ -193,6 +193,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/tequila/overlay/common
 
+$(call inherit-product, vendor/tequila/config/bootanimation.mk)
+
 $(call inherit-product, vendor/tequila/config/version.mk)
 
 $(call inherit-product, vendor/tequila/config/sepolicy.mk)

CircleOS/android_vendor_circle

config/common.mk
@@ -165,7 +165,12 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NetworkStackOverlay
 
+$(call inherit-product, vendor/circle/config/bootanimation.mk)
+
 $(call inherit-product, vendor/circle/config/version.mk)
+
+# Import flipendo/turbo_adapter sepolicy
+$(call inherit-product, vendor/circle/config/sepolicy.mk)
 
 # Inherit from GMS product config
 $(call inherit-product, vendor/gms/gms_full.mk)